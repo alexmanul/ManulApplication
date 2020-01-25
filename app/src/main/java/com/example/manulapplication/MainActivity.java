acbc6cf80e230f7e87ac6cd41114238e073d5ce6
alexmanul <aleksejs.marcena@gmail.com>
2020-01-25 23:38:28 +0200

    Initial commit

---
+++ app/src/main/java/com/example/manulapplication/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.manulapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText loginEditText;
+    private EditText passwordEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        loginEditText = findViewById(R.id.et_login);
+        passwordEditText = findViewById(R.id.et_password);
+        Button signInButton = findViewById(R.id.btn_sign_in);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String login = loginEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if (TextUtils.isEmpty(login)) {
+                    loginEditText.setError("Cannot be empty!");
+                    return;
+                } else {
+                    loginEditText.setError(null);
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    passwordEditText.setError("Cannot be empty!");
+                    return;
+                } else {
+                    passwordEditText.setError(null);
+                }
+
+//                String text = "Login: " + login + " Password: " + password;
+//                Toast.makeText(MainActivity.this, text, Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(MainActivity.this, MainActivityTwo.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
